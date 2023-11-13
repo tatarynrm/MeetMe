d2e7dd0e3de435d3f8ae5a4bdf0a0259b6b8a1d6
tatarynrm <tatarynrm@gmail.com>
2023-11-13 17:12:22 +0200

    ewqewq

--- controllers/web/sttistic.js
+++ controllers/web/sttistic.js
@@ -2,8 +2,21 @@ const pool = require("../../db/pool");
 
 const getAllUsersCount = async (req,res) =>{
     try {
-        const result = await pool.query(`select COUNT(*) from users`)
-        console.log(result.rows);
+        const result = await pool.query(`
+        SELECT
+            u.total_users,
+            COALESCE(b.boys_count, 0) AS boys_count,
+            COALESCE(g.girls_count, 0) AS girls_count
+        FROM
+            (SELECT COUNT(*) AS total_users FROM users) AS u
+        LEFT JOIN
+            (SELECT COUNT(*) AS boys_count FROM users_info WHERE sex = 'M') AS b
+        ON true
+        LEFT JOIN
+            (SELECT COUNT(*) AS girls_count FROM users_info WHERE sex = 'W') AS g
+        ON true
+    `);
+    console.log(result);
         res.json(result.rows)
     } catch (error) {
         console.log(error);
